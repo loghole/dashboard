cba7a176fb82cda243bf70c51ba18925621f514d
Gleb Dubrovin <termiusone@gmail.com>
2020-07-03 21:16:31 +0300

    update tests

--- collector/go.mod
+++ collector/go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lissteron/simplerr v0.4.0
 	github.com/spf13/viper v1.7.0
-	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/stretchr/testify v1.6.1
 	go.uber.org/zap v1.15.0
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
--- collector/internal/app/domain/entry_test.go
+++ collector/internal/app/domain/entry_test.go
@@ -1,296 +1,159 @@
 package domain
 
 import (
-	"log"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var data = []byte(`
-{
-   "time":1593709730877594291,
-   "namespace":"prod",
-   "source":"app_1",
-   "host":"127.100.0.1:50000",
-   "level":"debug",
-   "trace_id":"1c7fuhpo0ln2dcq",
-   "message":"read failed: some message 4",
-   "build_commit":"db957a22b3c1d6e508c0828917a5e14c572fb007",
-   "config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
-   "key1":[
-      1,
-      2,
-      3,
-      4,
-      5,
-      6
-   ],
-   "key2":[
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f"
-   ],
-   "boolKey": true,
-   "key3":{
-      "key4":{
-         "key5":[
-            11,
-            11,
-            11,
-            "WWW",
-			{"someKey": "someValue"}
-         ]
-      }
-   }
-}`)
-
 func TestEntry_UnmarshalJSON(t *testing.T) {
-	entry := &Entry{}
-
-	if err := entry.UnmarshalJSON(data); err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		name        string
+		data        []byte
+		wantErr     bool
+		expectedErr string
+		expectedRes *Entry
+	}{
+		{
+			name:    "TimePass",
+			data:    []byte(`{"time":1593709730877594291}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				Time:   time.Unix(0, 1593709730877594291),
+				Params: []byte(`{"time":1593709730877594291}`),
+			},
+		},
+		{
+			name:        "TimeError",
+			data:        []byte(`{"time": "2020-07-03T17:07:40Z"}`),
+			wantErr:     true,
+			expectedErr: "Value looks like Number/Boolean/None, but can't find its end: ',' or '}' symbol",
+		},
+		{
+			name:    "NamespacePass",
+			data:    []byte(`{"namespace":"PROD"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				Namespace: "prod",
+				Params:    []byte(`{"namespace":"PROD"}`),
+			},
+		},
+		{
+			name:    "SourcePass",
+			data:    []byte(`{"source":"app_1"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				Source: "app_1",
+				Params: []byte(`{"source":"app_1"}`),
+			},
+		},
+		{
+			name:    "HostPass",
+			data:    []byte(`{"host":"127.0.0.1:4222"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				Host:   "127.0.0.1:4222",
+				Params: []byte(`{"host":"127.0.0.1:4222"}`),
+			},
+		},
+		{
+			name:    "LevelPass",
+			data:    []byte(`{"level":"info"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				Level:  "info",
+				Params: []byte(`{"level":"info"}`),
+			},
+		},
+		{
+			name:    "TraceIDPass",
+			data:    []byte(`{"trace_id":"qwertyu123456"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				TraceID: "qwertyu123456",
+				Params:  []byte(`{"trace_id":"qwertyu123456"}`),
+			},
+		},
+		{
+			name:    "MessagePass",
+			data:    []byte(`{"message":"some Message"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				Message: "some message",
+				Params:  []byte(`{"message":"some Message"}`),
+			},
+		},
+		{
+			name:    "BuildCommitPass",
+			data:    []byte(`{"build_commit":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				BuildCommit: "130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
+				Params:      []byte(`{"build_commit":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec"}`),
+			},
+		},
+		{
+			name:    "ConfigHashPass",
+			data:    []byte(`{"config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec"}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				ConfigHash: "130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
+				Params:     []byte(`{"config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec"}`),
+			},
+		},
+		{
+			name:    "StringPass",
+			data:    []byte(`{"key":["a","b","c","d","e","f"]}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				StringKey: []string{"key", "key", "key", "key", "key", "key"},
+				StringVal: []string{"a", "b", "c", "d", "e", "f"},
+				Params:    []byte(`{"key":["a","b","c","d","e","f"]}`),
+			},
+		},
+		{
+			name:    "FloatPass",
+			data:    []byte(`{"key":[1, 2, 3, 4, 5, 6]}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				FloatKey: []string{"key", "key", "key", "key", "key", "key"},
+				FloatVal: []float64{1, 2, 3, 4, 5, 6},
+				Params:   []byte(`{"key":[1, 2, 3, 4, 5, 6]}`),
+			},
+		},
+		{
+			name:    "BigObjectPass",
+			data:    []byte(`{"key3":{"key4":{"key5":[11,12,13,"WWW",{"someKey": "someValue"}]}}}`),
+			wantErr: false,
+			expectedRes: &Entry{
+				StringKey: []string{"key5", "somekey"},
+				StringVal: []string{"www", "somevalue"},
+				FloatKey:  []string{"key5", "key5", "key5"},
+				FloatVal:  []float64{11, 12, 13},
+				Params:    []byte(`{"key3":{"key4":{"key5":[11,12,13,"WWW",{"someKey": "someValue"}]}}}`),
+			},
+		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			entry := &Entry{}
 
-	log.Println(entry.Time)
-	log.Println(entry.Namespace)
-	log.Println(entry.Source)
-	log.Println(entry.Host)
-	log.Println(entry.Level)
-	log.Println(entry.TraceID)
-	log.Println(entry.Message)
-	log.Println(entry.BuildCommit)
-	log.Println(entry.ConfigHash)
-	log.Println(string(entry.Params))
-	log.Println(entry.StringKey)
-	log.Println(entry.StringVal)
-	log.Println(entry.FloatKey)
-	log.Println(entry.FloatVal)
-}
+			err := entry.UnmarshalJSON(tt.data)
+			if (err != nil) != tt.wantErr {
+				t.Error(err)
+			}
 
-var data2 = []byte(`
-[{
-   "time":1593709730877594291,
-   "namespace":"prod",
-   "source":"app_1",
-   "host":"127.100.0.1:50000",
-   "level":"debug",
-   "trace_id":"1c7fuhpo0ln2dcq",
-   "message":"read failed: some message 4",
-   "build_commit":"db957a22b3c1d6e508c0828917a5e14c572fb007",
-   "config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
-   "key1":[
-      1,
-      2,
-      3,
-      4,
-      5,
-      6
-   ],
-   "key2":[
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f"
-   ],
-   "boolKey": true,
-   "key3":{
-      "key4":{
-         "key5":[
-            11,
-            11,
-            11,
-            "WWW",
-			{"someKey": "someValue"}
-         ]
-      }
-   }
-},
-{
-   "time":1593709730877594291,
-   "namespace":"prod",
-   "source":"app_1",
-   "host":"127.100.0.1:50000",
-   "level":"debug",
-   "trace_id":"1c7fuhpo0ln2dcq",
-   "message":"read failed: some message 4",
-   "build_commit":"db957a22b3c1d6e508c0828917a5e14c572fb007",
-   "config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
-   "key1":[
-      1,
-      2,
-      3,
-      4,
-      5,
-      6
-   ],
-   "key2":[
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f"
-   ],
-   "boolKey": true,
-   "key3":{
-      "key4":{
-         "key5":[
-            11,
-            11,
-            11,
-            "WWW",
-			{"someKey": "someValue"}
-         ]
-      }
-   }
-},
-{
-   "time":1593709730877594291,
-   "namespace":"prod",
-   "source":"app_1",
-   "host":"127.100.0.1:50000",
-   "level":"debug",
-   "trace_id":"1c7fuhpo0ln2dcq",
-   "message":"read failed: some message 4",
-   "build_commit":"db957a22b3c1d6e508c0828917a5e14c572fb007",
-   "config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
-   "key1":[
-      1,
-      2,
-      3,
-      4,
-      5,
-      6
-   ],
-   "key2":[
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f"
-   ],
-   "boolKey": true,
-   "key3":{
-      "key4":{
-         "key5":[
-            11,
-            11,
-            11,
-            "WWW",
-			{"someKey": "someValue"}
-         ]
-      }
-   }
-},
-{
-   "time":1593709730877594291,
-   "namespace":"prod",
-   "source":"app_1",
-   "host":"127.100.0.1:50000",
-   "level":"debug",
-   "trace_id":"1c7fuhpo0ln2dcq",
-   "message":"read failed: some message 4",
-   "build_commit":"db957a22b3c1d6e508c0828917a5e14c572fb007",
-   "config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
-   "key1":[
-      1,
-      2,
-      3,
-      4,
-      5,
-      6
-   ],
-   "key2":[
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f"
-   ],
-   "boolKey": true,
-   "key3":{
-      "key4":{
-         "key5":[
-            11,
-            11,
-            11,
-            "WWW",
-			{"someKey": "someValue"}
-         ]
-      }
-   }
-},
-{
-   "time":1593709730877594291,
-   "namespace":"prod",
-   "source":"app_1",
-   "host":"127.100.0.1:50000",
-   "level":"debug",
-   "trace_id":"1c7fuhpo0ln2dcq",
-   "message":"read failed: some message 4",
-   "build_commit":"db957a22b3c1d6e508c0828917a5e14c572fb007",
-   "config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec",
-   "key1":[
-      1,
-      2,
-      3,
-      4,
-      5,
-      6
-   ],
-   "key2":[
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f"
-   ],
-   "boolKey": true,
-   "key3":{
-      "key4":{
-         "key5":[
-            11,
-            11,
-            11,
-            "WWW",
-			{"someKey": "someValue"}
-         ]
-      }
-   }
-}]`)
-
-func TestEntryList_UnmarshalJSON(t *testing.T) {
-	list := EntryList{}
-
-	if err := list.UnmarshalJSON(data2); err != nil {
-		t.Fatal(err)
-	}
-
-	for _, val := range list {
-		log.Println(*val)
+			if !tt.wantErr {
+				assert.Equal(t, tt.expectedRes, entry)
+			}
+		})
 	}
 }
-
-func BenchmarkEntryList_UnmarshalJSON(b *testing.B) {
-	list := EntryList{}
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		if err := list.UnmarshalJSON(data2); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
 
 func BenchmarkEntry_UnmarshalJSON(b *testing.B) {
+	data := []byte(`{"time":1593709730877594291,"namespace":"prod","source":"app_1","host":"127.100.0.1:50000","level":"debug","trace_id":"1c7fuhpo0ln2dcq","message":"read failed: some message 4","build_commit":"db957a22b3c1d6e508c0828917a5e14c572fb007","config_hash":"130362a6fd10cf2f939dd0cfc0ab222cee6a99ec","key1":[1,2,3,4,5,6],"key2":["a","b","c","d","e","f"],"boolKey":true,"key3":{"key4":{"key5":[11,11,11,"WWW",{"someKey":"someValue"}]}}}`)
+
 	entry := &Entry{}
 	b.ReportAllocs()
 
