41c93f6c420d3979b746fadb919fd5ebd667eccf
lissteron <lissteron@gmail.com>
2020-09-14 11:56:49 +0300

    show version in frontend #6

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,3 +3,29 @@
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  methods: {
+    getVersion() {
+      this.axios
+        .get('/api/v1/info')
+        .then((response) => {
+          const info = response.data.data;
+
+          Object.keys(info).forEach((key) => {
+            console.log(`${key}: ${info[key]}`);
+          });
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    },
+  },
+  mounted() {
+    this.getVersion();
+  },
+});
+</script>
